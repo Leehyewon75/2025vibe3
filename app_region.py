a77af16114bd2920ab4f986d3a89caac393efbad
Leehyewon75 <kb202322@gmail.com>
2025-07-25 11:42:44 +0900

    Update app_region.py

--- app_region.py
+++ app_region.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-st.title("행정구역별 인구수 시각화 (설치 없이)")
+st.title("행정구역별 인구수 선그래프 (설치 없이)")
 
 uploaded_file = st.file_uploader("CSV 파일 업로드", type=["csv"])
 
 if uploaded_file:
     df = pd.read_csv(uploaded_file, encoding='cp949')
 
-    # 숫자형으로 변환 (쉼표 제거)
+    # 쉼표 제거 후 숫자 변환
     df["2025년06월_계_총인구수"] = df["2025년06월_계_총인구수"].str.replace(",", "").astype(int)
 
     st.dataframe(df.head())
 
     chart_data = df.set_index("행정구역")["2025년06월_계_총인구수"]
-    st.bar_chart(chart_data)
+    st.line_chart(chart_data)
 
 else:
     st.info("CSV 파일을 업로드 해주세요.")
