9193ad2ba954d2d812d40d31e12c64c6b052e331
Leehyewon75 <kb202322@gmail.com>
2025-07-25 11:43:33 +0900

    Update app_region.py

--- app_region.py
+++ app_region.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("행정구역별 인구수 선그래프 (설치 없이)")
+st.title("행정구역별 인구수 선그래프 (Matplotlib)")
 
 uploaded_file = st.file_uploader("CSV 파일 업로드", type=["csv"])
 
@@ -13,8 +14,14 @@ if uploaded_file:
 
     st.dataframe(df.head())
 
-    chart_data = df.set_index("행정구역")["2025년06월_계_총인구수"]
-    st.line_chart(chart_data)
+    plt.figure(figsize=(12,6))
+    plt.plot(df["행정구역"], df["2025년06월_계_총인구수"], marker='o', linestyle='-')
+    plt.xticks(rotation=90)
+    plt.ylabel("총인구수")
+    plt.title("행정구역별 총인구수 선그래프")
+    plt.tight_layout()
+
+    st.pyplot(plt)
 
 else:
     st.info("CSV 파일을 업로드 해주세요.")
